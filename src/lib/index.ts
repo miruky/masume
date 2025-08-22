7c149120fd8366ac980b0efbec6afc00e8536096
miruky <h.miyoki@gmail.com>
2025-08-22 15:59:53 +0900

    URL関連とfirstEmptyCellを公開APIへ加える

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,5 +2,7 @@ export { words } from './data/words';
 export type { WordEntry } from './data/words';
 export { createRng, shuffle } from './rng';
 export { normalizeCell, toKatakana } from './kana';
-export { generate, grade } from './generator';
+export { firstEmptyCell, generate, grade } from './generator';
 export type { Dir, GradeResult, Placement, Puzzle } from './generator';
+export { DEFAULT_SIZE, formatHash, MAX_SIZE, MIN_SIZE, parseHash } from './url';
+export type { PuzzleRoute } from './url';
